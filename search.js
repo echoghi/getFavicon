d82727c993ad558b703fe330cf18552625fa6e9c
Emile Choghi <emchoghi@gmail.com>
2016-06-06 23:37:43 -0700

    Update search.js

--- search.js
+++ search.js
@@ -14,10 +14,14 @@ $('.btn-shorten').on('click', function(){
     data: JSON.stringify({url: $('#url-field').val()}),
     contentType: "application/json; charset=utf-8",
     success: function(data){
-      console.log(data);
-        // Provide a favicon download link that is returned by the server
-        var resultHTML = '<a class="result" href="/download">' +
-            'Download main favicon from '+ data.searchUrl + '</a>';
+      // Provide a favicon download link that is returned by the server
+      var resultHTML = '<a class="result" href="/download">' +
+          'Download main favicon from '+ data.searchUrl + '</a>';
+
+      if(data.searchUrl === 'error'){
+        resultHTML = '<p class="result">No Favicon was found, please check the URL and try again.';
+      }
+console.log(data.searchUrl)
         $('#link').html(resultHTML);
         $('#link').hide().fadeIn('slow');
     }
